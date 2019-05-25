2e4e5abda213c6d1835bf390dbab9eca4340d1b8
pipobarari <51032435+pipobarari@users.noreply.github.com>
2019-05-25 13:49:42 -0600

    Add files via upload

---
+++ portafolio asientos.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class ViajeTren{
+private:
+	
+public:
+	ViajeTren(){
+		string ruta;
+		int hsalida;
+		int asientos;	
+	}
+	ViajeTren(string _ruta, int _hsalida, int _asientos){
+		ruta=_ruta;
+		hsalida=_hsalida;
+		asientos=_asientos;
+	}
+	ViajeTren(){
+		ruta="Desconocida.";
+		hsalida=0;
+		asientos=0;
+	}
+};
+
+int main(int argc, char *argv[]) {
+	
+	cout<<"					****Bienvenido a nuestro sitio web****"<<endl;
+	cout<<
+	
+	return 0;
+}
+
