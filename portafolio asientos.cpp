a945b83501c977866f3174caa26c22a9076a2a35
jererodri14 <51032438+jererodri14@users.noreply.github.com>
2019-05-25 15:42:00 -0600

    Update portafolio asientos.cpp

--- portafolio asientos.cpp
+++ portafolio asientos.cpp
@@ -4,23 +4,42 @@ using namespace std;
 
 class ViajeTren{
 private:
-	
+	string ruta;
+	int hsalida;
+	int asientos[10];
 public:
-	ViajeTren(){
-		string ruta;
-		int hsalida;
-		int asientos;	
-	}
-	ViajeTren(string _ruta, int _hsalida, int _asientos){
+	ViajeTren(string _ruta, int _hsalida){
 		ruta=_ruta;
 		hsalida=_hsalida;
-		asientos=_asientos;
+		asientos[]=for(int i=0;i<10;i++)
+		{asientos[i]=0;
+		}
 	}
 	ViajeTren(){
 		ruta="Desconocida.";
 		hsalida=0;
-		asientos=0;
+		asientos=for(int i=0;i<10;i++)
+		{asientos[i]=0;
+		}
 	}
+	void set ruta(int _ruta){
+		ruta=_ruta;
+	}
+	int get ruta(){
+		return ruta;
+	}
+	void set hsalida(_hsalida){
+		hsalida=_hsalida;
+	}
+	reservar(){
+		if (op==1)
+		{
+		
+		
+		}
+			
+	
+	
 };
 
 int main(int argc, char *argv[]) {
